3384aa78ff9c96e31b989a674ebd4c43bfdff14f
Pierre Baudoux <notxio@gmail.com>
2016-11-22 13:55:01 +0100

    Update test_alice.py
    Correction des bugs de cedriiiiic ousselem

--- src/lib/test_alice.py
+++ src/lib/test_alice.py
@@ -5,7 +5,7 @@ import configparser
 class test_alice:
     def __init__(self,name):
         config = configparser.ConfigParser()
-        config.read('../config/peers.ini')
+        config.read('../../config/peers.ini')
         self.name = name
         self.ip = config[name]['ip_address']
         self.port = int(config[name]['port_number'])
@@ -14,5 +14,9 @@ class test_alice:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect(('127.0.0.1', self.port))
             s.sendall(bytearray([1,4,0,6])+bytearray([0xa2f9fc503534324c035b8ff21a465a5b25726a1d >> i & 0xff for i in range(152,-1,-8)]))
-            data = s.recv(1024)
-        print('Received', repr(data))
+            data = s.recv(5120000)
+        #print('Received', repr(data))
+        print(len(data))
+
+test = test_alice('bob')
+test.start()
