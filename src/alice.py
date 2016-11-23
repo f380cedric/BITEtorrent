7be945bcfe3b7544b6a093013f975acaa95e0f19
Pierre Baudoux <notxio@gmail.com>
2016-11-23 13:31:44 +0100

    No explaination in alice.py
    The explanation are given in peers.py

--- src/alice.py
+++ src/alice.py
@@ -1,32 +1,4 @@
-# Alice act as a server, she'll provide on request the chunks that Charlie need.
-
-# She needs to read the config/peers.ini file to know her IP address and port number.
-
-# To know wich chunks she have, alice need to read the /chunks/alice/*.bin files.
-
-# She'll listen for incomming request on her IP/port address.
-
-# When she receive a message :
-
-####### If the message is invalid : ERROR INVALID_MESSAGE_FORMAT
-# ie : | 1 | 6 | ? | INVALID_MESSAGE_FORMAT |
-
-####### If the message is not a GET_CHUNK : ERROR  INVALID_REQUEST
-
-####### If she doesn't have the chunk : ERROR CHUNK_NOT_FOUND
-## (look to the directory content) !!
-
-####### Otherwise : CHUNK message
-
-# Known problems :
-# 
-# sendall or send ?
-# Accept one connection at a time (=> multithreading ?)
-# Stop listening after one connection (=> while True:)
-#
-# ../config/peers.ini => rootpath
 from lib.peers import peers
 
 alice = peers('alice')
 alice.start()
-#alice.generate_chunk_message(0xa2f9fc503534324c035b8ff21a465a5b25726a1d)
