8270f24ee8114c354cbb9927f139c3edf27d34a1
Pierre Baudoux <notxio@gmail.com>
2016-11-29 17:25:59 +0100

    Revert "sketch of the threading"
    This reverts commit 914bd0be88d1b212c68f053a193967554cb2cd0e.

--- src/lib/client.py
+++ src/lib/client.py
@@ -2,29 +2,11 @@ import os
 import socket
 import configparser
 import struct
-import threading
 
-# mychunks = os.listdir('../chunks/charlie')
-# config = configparser.ConfigParser()
-# config.read('../config/file.ini')
-# chunks = {}
-# for key in config['chunks']:
-#     if config['chunks'][key]+".bin" not in mychunks:
-#         chunks[config['chunks'][key]] = (config['chunks_peers'].get(key)).split(',')
-
-class client(threading.Thread):
-    def __init__(self,n):
-        threading.Thread.__init__(self)
-        self.n = n
-
-    def run(self):
-        while True:
-            lock.acquire()
-            print('Je suis le thread', self.n)
-            lock.release()
-
-lock = threading.Lock()
-t1 = client(1)
-t2 = client(2)
-t1.start()
-t2.start()
+mychunks = os.listdir('../chunks/charlie')
+config = configparser.ConfigParser()
+config.read('../config/file.ini')
+chunks = {}
+for key in config['chunks']:
+    if config['chunks'][key]+".bin" not in mychunks:
+        chunks[config['chunks'][key]] = (config['chunks_peers'].get(key)).split(',')
