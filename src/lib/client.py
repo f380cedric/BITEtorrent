54cdbd139486377cefefca391cbb42b0aeb83f54
f380cedric <f380cedric@users.noreply.github.com>
2016-12-02 10:07:36 +0100

    testing Catch the ERROR CHUNK_NOT_FOUND (WIP) to see if client can send more than one message to the peer

--- src/lib/client.py
+++ src/lib/client.py
@@ -27,12 +27,13 @@ class client:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as self.s:
             # JUST FOR TESTING
             self.s.connect(self.addresses[self.chunks['6e14c28263c0b81a4bb70ddbc3c504be5bc8f4e8'][0]])
-            self.chunk_request('6e14c28263c0b81a4bb70ddbc3c504be5bc8f4e8',self.addresses[self.chunks['6e14c28263c0b81a4bb70ddbc3c504be5bc8f4e8'][0]])
+            result = self.chunk_request('6b14c28263c0b81a4bb70ddbc3c504be5bc8f4e8',self.addresses[self.chunks['6e14c28263c0b81a4bb70ddbc3c504be5bc8f4e8'][0]])
+            if struct.unpack("!BBHLHH",result)[1] == 6 :
+                self.chunk_request('6e14c28263c0b81a4bb70ddbc3c504be5bc8f4e8',self.addresses[self.chunks['6e14c28263c0b81a4bb70ddbc3c504be5bc8f4e8'][0]])
 
     def chunk_request(self, chunk_hash, client):
         self.s.send(self.chunk_message_generator(chunk_hash))
-        data = self.s.recv(5120000)
-        print(data)
+        return self.s.recv(5120000)
         # WIP
 
     def chunk_message_generator(self,chunk_hash):
