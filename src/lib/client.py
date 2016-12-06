bb18db8bfffb3a2932e8ab14c3f1198fb14771f1
f380cedric <f380cedric@users.noreply.github.com>
2016-12-06 16:40:12 +0100

    client.chunks changed

--- src/lib/client.py
+++ src/lib/client.py
@@ -19,31 +19,34 @@ class client(threading.Thread):
 
         config = configparser.ConfigParser()
         config.read('../config/peers.ini')
-
         self.addresses = {}
         for key in config.sections():
             self.addresses[key] = (config[key]['ip_address'], int(config[key]['port_number']))
 
-        mychunks = os.listdir('../chunks/'+self.name)
-
-        config = configparser.ConfigParser()
-
+        mychunks = os.listdir('../chunks/' +name)
         config.read('../config/file.ini')
-        self.chunks = {}
+        self.chunks = {'alice':[],'bob':[],'alice, bob':[]}
         for key in config['chunks']:
             if config['chunks'][key]+".bin" not in mychunks:
-                self.chunks[config['chunks'][key]] = tuple((config['chunks_peers'].get(key)).split(','))
-
-
+                self.chunks[config['chunks_peers'].get(key)].append(config['chunks'][key])
+        print(self.chunks)
+        #self.lock
     def run(self):
-        #while True:
+        return
+        while True:
+
+            """
         lock.acquire()
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as self.s:
             # JUST FOR TESTING
+            for key in self.chunks:
+                peer = self.chunks
+
             self.s.connect(self.addresses[self.chunks['6e14c28263c0b81a4bb70ddbc3c504be5bc8f4e8'][0]])
             result = self.chunk_request('6b14c28263c0b81a4bb70ddbc3c504be5bc8f4e8',self.addresses[self.chunks['6e14c28263c0b81a4bb70ddbc3c504be5bc8f4e8'][0]])
             if struct.unpack("!BBHLHH",result)[1] == 6 :
                 self.chunk_request('6e14c28263c0b81a4bb70ddbc3c504be5bc8f4e8',self.addresses[self.chunks['6e14c28263c0b81a4bb70ddbc3c504be5bc8f4e8'][0]])
+                """
         lock.release()
 
     def chunk_request(self, chunk_hash, client):
@@ -61,4 +64,3 @@ class client(threading.Thread):
         message_content = struct.pack(fmt_content,bytes.fromhex(chunk_hash))
 
         return header+message_content
-lock = threading.Lock()
