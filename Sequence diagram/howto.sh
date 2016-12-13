83cb686f6410ae39442ab9db29909d1df4ecf00c
Pierre Baudoux <notxio@gmail.com>
2016-12-13 18:38:57 +0100

    shell script now generate all the sequence diagram

--- Sequence diagram/howto.sh
+++ Sequence diagram/howto.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 java -jar plantuml.jar -teps step1.txt -o "../Report/img"
+java -jar plantuml.jar -teps step2.txt -o "../Report/img"
+java -jar plantuml.jar -teps step3.txt -o "../Report/img"
