b230dbdcd449db9e4340ae2f84f79917b39a7115
Pierre Baudoux <pbaudoux@ulb.ac.be>
2016-12-18 09:02:50 +0100

    delete comment from the statement

--- src/lib/peer.py
+++ src/lib/peer.py
@@ -1,30 +1,3 @@
-# Alice act as a server, she'll provide on request the chunks that Charlie need.
-
-# She needs to read the config/peers.ini file to know her IP address and port number.
-
-# To know wich chunks she have, alice need to read the /chunks/alice/*.bin files.
-
-# She'll listen for incomming request on her IP/port address.
-
-# When she receive a message :
-
-####### If the message is invalid : ERROR INVALID_MESSAGE_FORMAT
-# ie : | 1 | 6 | ? | INVALID_MESSAGE_FORMAT |
-
-####### If the message is not a GET_CHUNK : ERROR  INVALID_REQUEST
-
-####### If she doesn't have the chunk : ERROR CHUNK_NOT_FOUND
-## (look to the directory content) !!
-
-####### Otherwise : CHUNK message
-
-# Known problems :
-#
-# sendall or send ?
-# Accept one connection at a time (=> multithreading ?)
-# Stop listening after one connection (=> while True:)
-#
-# ../config/peers.ini => rootpath
 import os
 import socket
 import configparser
