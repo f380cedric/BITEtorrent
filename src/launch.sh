117a98abe6d06ad2f8c6355ecd5c25ff2bc5491c
f380cedric <f380cedric@users.noreply.github.com>
2016-12-18 16:07:53 +0100

    improvment of launch.sh

--- src/launch.sh
+++ src/launch.sh
@@ -5,7 +5,23 @@
 
 ### STEP 1 ###
 
-if [ "$1" = "1" ]; then
+
+if hash x-terminal-emulator 2>/dev/null; then
+    console=x-terminal-emulator
+elif hash gnome-terminal 2>/dev/null; then
+    console=gnome-terminal
+elif hash konsole 2>/dev/null; then
+    console=konsole
+elif hash xterm 2>/dev/null; then
+    console=xterm
+elif hash xfce4-terminal 2>/dev/null; then
+    console=xfce4-terminal
+else
+    echo "Can't found terminal"
+    exit
+fi
+
+if [ $1 = 1 ]; then
     echo "Lauching step 1 of the project:"
     
     echo "Clearing chunks/charlie"
@@ -14,10 +30,10 @@ if [ "$1" = "1" ]; then
     echo "Checking if alice is launched:"
     netcat -w 3 -z 127.0.0.1 50007
     
-    if [ "$?" = "1" ]; then
+    if [ $? = 1 ]; then
         echo "alice not launched"
         echo "Lauching alice"
-        x-terminal-emulator -e $(pwd)/exec.sh python3 alice.py
+        $console -e  'python3 alice.py'
     else
         echo "alice already launched"
     fi
@@ -25,10 +41,10 @@ if [ "$1" = "1" ]; then
     echo "Checking if bob is launched:"
     netcat -w 3 -z 127.0.0.2 50007
     
-    if [ "$?" = "1" ]; then
+    if [ $? = 1 ]; then
         echo "bob not launched"
         echo "Lauching bob"
-        x-terminal-emulator -e $(pwd)/exec.sh python3 bob.py
+        $console -e 'python3 bob.py'
     else
         echo "bob already launched"
     fi
@@ -39,7 +55,7 @@ if [ "$1" = "1" ]; then
 
 ### STEP 2 ###
 
-elif [ "$1" = "2" ]; then
+elif [ $1 = 2 ]; then
     echo "Lauching step 2 of the project:"
     
     echo "Clearing chunks/charlie"
@@ -48,10 +64,10 @@ elif [ "$1" = "2" ]; then
     echo "Checking if alice is launched:"
     netcat -w 3 -z 127.0.0.1 50007
 
-    if [ "$?" = "1" ]; then
+    if [ $? = 1 ]; then
         echo "alice not launched"
         echo "Lauching alice"
-        x-terminal-emulator -e $(pwd)/exec.sh python3 alice.py
+        $console -e $(pwd)/exec.sh python3 alice.py
     else
         echo "alice already launched"
     fi
@@ -59,10 +75,10 @@ elif [ "$1" = "2" ]; then
     echo "Checking if bob is launched:"
     netcat -w 3 -z 127.0.0.2 50007
     
-    if [ "$?" = "1" ]; then
+    if [ $? = 1 ]; then
         echo "bob not launched"
         echo "Lauching bob"
-        x-terminal-emulator -e $(pwd)/exec.sh python3 bob.py
+        $console -e $(pwd)/exec.sh python3 bob.py
     else
         echo "bob already launched"
     fi
@@ -70,10 +86,10 @@ elif [ "$1" = "2" ]; then
     echo "Checking if tracker is launched:"
     netcat -w 3 -z 127.0.0.1 8000
     
-    if [ "$?" = "1" ]; then
+    if [ $? = 1 ]; then
         echo "tracker not launched"
         echo "Lauching tracker"
-        x-terminal-emulator -e $(pwd)/exec.sh python3 tracker.py
+        $console -e $(pwd)/exec.sh python3 tracker.py
     else
         echo "tracker already launched"
     fi
@@ -84,7 +100,7 @@ elif [ "$1" = "2" ]; then
 
 ### STEP 3 ###
 
-elif [ "$1" = "3" ]; then
+elif [ $1 = 3 ]; then
 	echo "Feature not yet implemented"
 
 else
